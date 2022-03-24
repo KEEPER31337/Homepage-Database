1b43aa2f79a74816a92756fbec58023ba02c9844
shasuri <ghimmk@gmail.com>
2022-03-24 22:09:44 +0000

    keeper_db 0.33.0 - 20220324 DB 패치노트 - 기본 값이 수정되었습니다.     - `member_job`         - `badge_thumbnail_id`의 기본 값을 각 직책에 맞게 업데이트 했습니다. - 배포서버  `update.sql`는 제가 적용하겠습니당~

---
+++ 2022-03-24/20220324_keeper_update.sql
@@ -0,0 +1,8 @@
+UPDATE `member_job` SET `badge_thumbnail_id` = 11 WHERE `id` = 1;
+UPDATE `member_job` SET `badge_thumbnail_id` = 12 WHERE `id` = 2;
+UPDATE `member_job` SET `badge_thumbnail_id` = 13 WHERE `id` = 3;
+UPDATE `member_job` SET `badge_thumbnail_id` = 14 WHERE `id` = 4;
+UPDATE `member_job` SET `badge_thumbnail_id` = 15 WHERE `id` = 5;
+UPDATE `member_job` SET `badge_thumbnail_id` = 16 WHERE `id` = 6;
+UPDATE `member_job` SET `badge_thumbnail_id` = 17 WHERE `id` = 7;
+UPDATE `member_job` SET `badge_thumbnail_id` = 18 WHERE `id` = 8;
