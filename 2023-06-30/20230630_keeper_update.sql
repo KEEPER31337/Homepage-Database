1e987397fc811dde5f119fdc5810849a84988cb5
shasuri <ghimmk@gmail.com>
2023-07-01 02:12:17 +0900

    + update 2023-06-30/20230630_keeper_update.sql

--- 2023-06-30/20230630_keeper_update.sql
+++ 2023-06-30/20230630_keeper_update.sql
@@ -1,14 +1,5 @@
 USE `keeper`;
 
--- Remove foreign key constraints and columns.
-ALTER TABLE
-    file DROP FOREIGN KEY `fk_file_posting1`,
-    DROP COLUMN `posting_id`;
-
-ALTER TABLE
-    ctf_challenge DROP FOREIGN KEY `fk_ctf_challenge_file1`,
-    DROP COLUMN `file_id`;
-
 -- -----------------------------------------------------
 -- Table `keeper`.`posting_has_file`
 -- -----------------------------------------------------
@@ -36,3 +27,35 @@ CREATE TABLE IF NOT EXISTS `keeper`.`ctf_challenge_has_file` (
     CONSTRAINT `fk_ctf_challenge_has_file_ctf_challenge1` FOREIGN KEY (`ctf_challenge_id`) REFERENCES `keeper`.`ctf_challenge` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
     CONSTRAINT `fk_ctf_challenge_has_file_file1` FOREIGN KEY (`file_id`) REFERENCES `keeper`.`file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
+
+-- Move data to new tables.
+INSERT INTO
+    `posting_has_file` (
+        SELECT
+            `posting_id`,
+            `id` as file_id
+        FROM
+            `file`
+        WHERE
+            `posting_id` IS NOT NULL
+    );
+
+INSERT INTO
+    `ctf_challenge_has_file` (
+        SELECT
+            `id` as ctf_challenge_id,
+            `file_id`
+        FROM
+            `ctf_challenge`
+        WHERE
+            `file_id` IS NOT NULL
+    );
+
+-- Remove foreign key constraints and columns.
+ALTER TABLE
+    file DROP FOREIGN KEY `fk_file_posting1`,
+    DROP COLUMN `posting_id`;
+
+ALTER TABLE
+    ctf_challenge DROP FOREIGN KEY `fk_ctf_challenge_file1`,
+    DROP COLUMN `file_id`;
