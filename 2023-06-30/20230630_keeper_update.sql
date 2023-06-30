5da77af41c18f29f9a4f3c617e5723d1b9bac18a
shasuri <ghimmk@gmail.com>
2023-06-30 23:53:49 +0900

    20230630 DB 업데이트
    - `file`, `posting`, `ctf_challenge`의 관계를 개편 했습니다.
    - `posting` - `file` : 1:N -> N:M
    - `ctf_challenge` - `file` : 1:1 -> N:M
    - 이제 ctf 문제는 여러개의 첨부파일을 가질 수 있습니다.
    
    - 이에 컬럼/관계가 제거 되었습니다.
      - `file.posting_id` 제거
            - 대신 `posting_has_file` 테이블이 추가되었습니다.
                    - 컬럼으로 `posting_id`, `file_id`를 가지게 됩니다.
      - `ctf_challnge.file_id` 제거
            - `ctf_challenge_has_file` 테이블이 추가되었습니다.
                    - 컬럼으로 `ctf_challenge_id`, `file_id`를 가지게 됩니다.

---
+++ 2023-06-30/20230630_keeper_update.sql
@@ -0,0 +1,38 @@
+USE `keeper`;
+
+-- Remove foreign key constraints and columns.
+ALTER TABLE
+    file DROP FOREIGN KEY `fk_file_posting1`,
+    DROP COLUMN `posting_id`;
+
+ALTER TABLE
+    ctf_challenge DROP FOREIGN KEY `fk_ctf_challenge_file1`,
+    DROP COLUMN `file_id`;
+
+-- -----------------------------------------------------
+-- Table `keeper`.`posting_has_file`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `keeper`.`posting_has_file` (
+    `posting_id` INT NOT NULL,
+    `file_id` INT NOT NULL,
+    PRIMARY KEY (`posting_id`, `file_id`),
+    INDEX `fk_posting_has_file_file1_idx` (`file_id` ASC) VISIBLE,
+    INDEX `fk_posting_has_file_posting1_idx` (`posting_id` ASC) VISIBLE,
+    CONSTRAINT `fk_posting_has_file_posting1` FOREIGN KEY (`posting_id`) REFERENCES `keeper`.`posting` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_posting_has_file_file1` FOREIGN KEY (`file_id`) REFERENCES `keeper`.`file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `keeper`.`ctf_challenge_has_file`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `keeper`.`ctf_challenge_has_file`;
+
+CREATE TABLE IF NOT EXISTS `keeper`.`ctf_challenge_has_file` (
+    `ctf_challenge_id` INT NOT NULL,
+    `file_id` INT NOT NULL,
+    PRIMARY KEY (`ctf_challenge_id`, `file_id`),
+    INDEX `fk_ctf_challenge_has_file_file1_idx` (`file_id` ASC) VISIBLE,
+    INDEX `fk_ctf_challenge_has_file_ctf_challenge1_idx` (`ctf_challenge_id` ASC) VISIBLE,
+    CONSTRAINT `fk_ctf_challenge_has_file_ctf_challenge1` FOREIGN KEY (`ctf_challenge_id`) REFERENCES `keeper`.`ctf_challenge` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_ctf_challenge_has_file_file1` FOREIGN KEY (`file_id`) REFERENCES `keeper`.`file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
--- README.md
+++ README.md
@@ -4,11 +4,20 @@
 
 ## Recent Patch note
 
-### keeper_db 1.20.0-renew7.0
+### keeper_db 1.20.0-renew8.0
 
-20230530 DB 업데이트
+20230630 DB 업데이트
 
-- 컬럼이 추가/제거 되었습니다.
-  - `game_member_info`
-    - `baseball_per_day`, `baseball_day_point`, `play_date` 가 추가되었습니다.
-  - `study`.`member_number` 캐싱용도의 컬럼이 제거되었습니다.
+- `file`, `posting`, `ctf_challenge`의 관계를 개편 했습니다.
+- `posting` - `file` : 1:N -> N:M
+- `ctf_challenge` - `file` : 1:1 -> N:M
+- 이제 ctf 문제는 여러개의 첨부파일을 가질 수 있습니다.
+
+- 이에 컬럼/관계가 제거 되었습니다.
+  - `file.posting_id` 제거
+	- 대신 `posting_has_file` 테이블이 추가되었습니다.
+		- 컬럼으로 `posting_id`, `file_id`를 가지게 됩니다.
+  - `ctf_challnge.file_id` 제거
+	- `ctf_challenge_has_file` 테이블이 추가되었습니다.
+		- 컬럼으로 `ctf_challenge_id`, `file_id`를 가지게 됩니다.
+
